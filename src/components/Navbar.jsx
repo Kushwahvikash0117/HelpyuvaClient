2186fd117a794189319ef002f5df94c86b98e397
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-10-23 09:43:40 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           className="w-10 h-10 sm:w-12 sm:h-12 rounded-full shadow-md"
         />
         <h1 className="text-xl sm:text-2xl font-bold text-[#3D7072]">
-          Helpyuva Foundation
+          Helpयुवा
         </h1>
       </div>
 
